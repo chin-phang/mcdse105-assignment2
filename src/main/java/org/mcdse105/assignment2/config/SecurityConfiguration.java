a5b7b2044894a4fb39966fe58c855034425be5a5
Aaron Lee <chinphang.lee@hotmail.com>
2024-11-20 12:15:16 +0800

    Refactored user details service

--- src/main/java/org/mcdse105/assignment2/config/SecurityConfiguration.java
+++ src/main/java/org/mcdse105/assignment2/config/SecurityConfiguration.java
@@ -1,19 +1,50 @@
 package org.mcdse105.assignment2.config;
 
 import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
+import org.mcdse105.assignment2.repository.UserRepository;
+import org.mcdse105.assignment2.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl(this.userRepository);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider daoAuthProvider = new DaoAuthenticationProvider();
+        daoAuthProvider.setUserDetailsService(userDetailsService());
+        daoAuthProvider.setPasswordEncoder(passwordEncoder());
+
+        return new ProviderManager(daoAuthProvider);
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
---
+++ src/main/java/org/mcdse105/assignment2/service/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package org.mcdse105.assignment2.service;
+
+import lombok.RequiredArgsConstructor;
+import org.mcdse105.assignment2.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
